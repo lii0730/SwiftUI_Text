eb7803630d974a3ef636e13dedb9e265a66e2a8e
LeeHsss <lii0730@icloud.com>
2022-01-02 18:50:20 +0900

    Initial Commit

---
+++ SwiftUI_Text/SwiftUI_TextApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TextApp.swift
+//  SwiftUI_Text
+//
+//  Created by LeeHsss on 2022/01/02.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
